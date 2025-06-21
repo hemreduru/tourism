3ef05052a0214cb8778557e403f4253e91985c15
Emre <hemreduru@gmail.com>
2025-06-21 17:00:51 +0300

    SEO

--- resources/views/theme/head.blade.php
+++ resources/views/theme/head.blade.php
@@ -1,6 +1,25 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    {{-- SEO Meta Tags --}}
+    <meta name="description" content="@yield('meta_description', 'Echt Zorg Travel offers leading health tourism and partner services in Turkey and Europe.')">
+    <meta name="keywords" content="@yield('meta_keywords', 'health tourism, medical travel, surgery in turkey, medical tourism')">
+    <meta name="robots" content="@yield('meta_robots', 'index, follow')">
+    <link rel="canonical" href="{{ url()->current() }}"/>
+
+    {{-- OpenGraph / Facebook --}}
+    <meta property="og:type" content="@yield('og_type', 'website')">
+    <meta property="og:title" content="@yield('og_title', View::hasSection('title') ? trim(View::yieldContent('title')) : 'Echt Zorg Travel')">
+    <meta property="og:description" content="@yield('og_description', View::hasSection('meta_description') ? trim(View::yieldContent('meta_description')) : 'Echt Zorg Travel offers leading health tourism and partner services.')">
+    <meta property="og:url" content="{{ url()->current() }}">
+    <meta property="og:image" content="@yield('og_image', asset('assets/img/favicons/android-chrome-512x512.png'))">
+
+    {{-- Twitter --}}
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="@yield('og_title', View::hasSection('title') ? trim(View::yieldContent('title')) : 'Echt Zorg Travel')">
+    <meta name="twitter:description" content="@yield('og_description', View::hasSection('meta_description') ? trim(View::yieldContent('meta_description')) : 'Echt Zorg Travel offers leading health tourism and partner services.')">
+    <meta name="twitter:image" content="@yield('og_image', asset('assets/img/favicons/android-chrome-512x512.png'))">
+
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title', 'Echt Zorg Travel | Health Tourism')</title>
 
@@ -26,5 +45,6 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Fjalla+One&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100&display=swap" rel="stylesheet">
 
+    @stack('meta')
     @stack('styles')
 </head>
