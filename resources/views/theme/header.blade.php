ee09e412cacd844046ed0b87f6c0237765448519
Emre <hemreduru@gmail.com>
2025-06-23 23:19:05 +0300

    add Font Awesome icons to header for improved navigation aesthetics

--- resources/views/theme/header.blade.php
+++ resources/views/theme/header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark fixed-top py-h3 d-block" data-navbar-on-scroll="data-navbar-on-scroll">
     <div class="container">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" integrity="sha512-..." crossorigin="anonymous" referrerpolicy="no-referrer" />
+            <link rel="stylesheet" href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" integrity="sha512-..." crossorigin="anonymous" referrerpolicy="no-referrer" />
 
         <a class="navbar-brand" href="{{ url('/') }}">
             <img src="{{ asset('assets/img/logo/echt-zorg.logo.png') }}" width="250" alt="Echt Zorg Travel" />
