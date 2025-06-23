95cad736ffdd49557f74c8544108bc52a06008cb
Emre <hemreduru@gmail.com>
2025-06-23 18:52:59 +0300

    cookie fix

--- resources/views/theme/app.blade.php
+++ resources/views/theme/app.blade.php
@@ -10,7 +10,16 @@
     @include('theme.partials.top-footer')
     @include('theme.footer')
     @stack('scripts')
-    <script src="{{ asset('js/cookieConsent.js') }}"></script>
+
+    <!-- JavaScripts -->
+    <script src="{{ asset('vendors/@popperjs/popper.min.js') }}"></script>
+    <script src="{{ asset('vendors/bootstrap/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('vendors/is/is.min.js') }}"></script>
+    <script src="https://scripts.sirv.com/sirvjs/v3/sirv.js"></script>
+    <script src="{{ asset('vendors/fontawesome/all.min.js') }}"></script>
+    <script src="{{ asset('assets/js/theme.js') }}"></script>
+    <script src="{{ asset('js/buneyuv.js') }}"></script>
+
     <script>
         window.addEventListener("load", function () {
             window.cookieconsent.initialise({!! json_encode([
@@ -29,13 +38,5 @@
             ]) !!});
         });
     </script>
-    <!-- JavaScripts -->
-    <script src="{{ asset('vendors/@popperjs/popper.min.js') }}"></script>
-    <script src="{{ asset('vendors/bootstrap/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('vendors/is/is.min.js') }}"></script>
-    <script src="https://scripts.sirv.com/sirvjs/v3/sirv.js"></script>
-    <script src="{{ asset('vendors/fontawesome/all.min.js') }}"></script>
-    <script src="{{ asset('assets/js/theme.js') }}"></script>
-
 </body>
 </html>
